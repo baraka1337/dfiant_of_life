1c1d3aa792571b3235da193f5151eb19a4d1016c
Barak Aharoni <1999baraka@gmail.com.com>
2023-03-05 22:19:13 +0200

    Changes after talk.

--- src/main/scala/ID.scala
+++ src/main/scala/ID.scala
@@ -1,12 +1,19 @@
 import dfhdl.*
 
-class ID extends DFDesign:
-  val x = UInt(8) <> IN init 0
-  val y = UInt(8) <> OUT
-  y := x.prev + 1
+class ID extends EDDesign:
+    val x = UInt(8) <> IN
+    val y = UInt(8) <> OUT
+    y <> x
 
 @main def hello: Unit =
-  println("Hello, welcome to the DFHDL demo!")
-  println("Printing the top:")
-  val top = new TopLife
-  top.printCodeString
+    println("Hello, welcome to the DFHDL demo!")
+    println("Printing the top:")
+    import backends.verilog.sv2005
+    val top = new ID
+    // val top = new TopLife
+    import compiler.stages.StageRunner
+    StageRunner.logDebug()
+    top.compile
+        .toFolder("sandbox")
+        .printGenFiles
+        .lint
--- src/main/scala/PLL.scala
+++ src/main/scala/PLL.scala
@@ -1,8 +1,8 @@
 import dfhdl.*
 
-class PLL extends EDDesign:
-  val areset = Bit <> IN
-  val inclk0 = Bit <> IN
-  val c0 = Bit <> OUT
-  val c1 = Bit <> OUT
-  val locked = Bit <> OUT
+class PLL extends EDBlackBox(EDBlackBox.Source.NA, EDBlackBox.Source.NA):
+    val areset = Bit <> IN
+    val inclk0 = Bit <> IN
+    val c0     = Bit <> OUT
+    val c1     = Bit <> OUT
+    val locked = Bit <> OUT
